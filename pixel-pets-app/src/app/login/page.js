8e0de91183858f3537bce6def7c7ed4367cf58e8
mpelot <mwp81301@uga.edu>
2024-04-03 11:09:50 -0400

    basic login page

--- pixel-pets-app/src/app/login/page.js
+++ pixel-pets-app/src/app/login/page.js
@@ -3,7 +3,20 @@ import styles from "./login.css";
 export default function Home() {
   return (
     <div>
-      <h1>Login</h1>
+      <div class="login-container">
+        <h2>Login</h2>
+        <form action="#" method="post">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" required/>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required/>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    </div>
     </div>
   );
 }
