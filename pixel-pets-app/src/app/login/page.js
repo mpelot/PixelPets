aa6d5c61ab017b3ac4849f4c291acbcf40c4add2
mpelot <mwp81301@uga.edu>
2024-04-04 18:32:39 -0400

    fancy background

--- pixel-pets-app/src/app/login/page.js
+++ pixel-pets-app/src/app/login/page.js
@@ -1,25 +1,29 @@
 import styles from "./login.css";
+import bg from '../../../public/LogoBG.png'
 
 export default function Home() {
   return (
-    <div>
-      <div className="title">
-        <img src="logo.png"/>
-        <h1>PixelPets</h1>
-      </div>
-      <div className="login-container">
-        <h2>Login</h2>
-        <form action="home" method="post">
-            <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="username" required/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" required/>
-            </div>
-            <button type="submit">Login</button>
-        </form>
+      <div className="content">
+        <div className="background" style={{backgroundImage: `url(${bg.src})`}}></div>
+        <div className="login">
+          <div className="title">
+            <img src="logo.png"/>
+            <h1>PixelPets</h1>
+          </div>
+          <div className="login-container">
+            <h2>Login</h2>
+            <form action="home" method="post">
+                <div className="form-group">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" name="username" required/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" required/>
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
       </div>
     </div>
   );
