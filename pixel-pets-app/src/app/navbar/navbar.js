11f5bf795736064c32a000aca30c921d84a971ea
Max Pelot <mwp81301@uga.edu>
2024-04-04 22:33:52 -0400

    more setup

---
+++ pixel-pets-app/src/app/navbar/navbar.js
@@ -0,0 +1,9 @@
+import styles from "./navbar.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
+}
---
+++ pixel-pets-app/src/app/petCard/petCard.js
@@ -0,0 +1,9 @@
+import styles from "./petCard.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
+}
