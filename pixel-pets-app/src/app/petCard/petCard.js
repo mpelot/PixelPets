daa0978f6c308b126b93129fadbe1e0ea3d4129c
Max Pelot <mwp81301@uga.edu>
2024-04-06 15:56:33 -0400

    going overboard with effects

--- pixel-pets-app/src/app/petCard/petCard.js
+++ pixel-pets-app/src/app/petCard/petCard.js
@@ -7,6 +7,7 @@ export default function PetCard(props) {
     const [mouseX, setMouseX] = useState();
     const [mouseY, setMouseY] = useState();
     const [scale, setScale] = useState(1);
+    const [shadow, setShadow] = useState(0);
     const boundsRef = useRef(null);
     const cardRef = useRef(null);
 
@@ -17,9 +18,11 @@ export default function PetCard(props) {
         };
         const handleMouseEnter = async (e) => {
             setScale(1.1);
+            setShadow(1);
         };
         const handleMouseLeave = async (e) => {
             setScale(1)
+            setShadow(0);
             setMouseX(e.clientX);
             setMouseY(e.clientY);
         };
@@ -50,14 +53,28 @@ export default function PetCard(props) {
         return ((axis === 'X' ? mouseY : mouseX) - (axis === 'X' ? rect.top : rect.left) - (size === 'height' ? rect.height / 2 : rect.width / 2)) / 8;
     };
 
+    const calculateSheen = () => {
+        return (-rotateX + rotateY) * 10 + 50;
+    }
+
+    const calculateSheenOpacity = () => {
+        if (!boundsRef.current) return 0;
+        const rect = boundsRef.current.getBoundingClientRect();
+        if (mouseX < rect.left ||  mouseX > rect.right || mouseY > rect.bottom || mouseY < rect.top) return 0;
+        return (-(Math.abs(rotateX) + Math.abs(rotateY)) + 13) / 50;
+    }
+
     const rotateX = calculateRotate('X', 'height');
     const rotateY = calculateRotate('Y', 'width');  
+    const sheenPosition = calculateSheen();
+    const sheenOpacity = calculateSheenOpacity();
 
     return (    
         <div className='container' ref={boundsRef}>
-            <div className='rotationWrapper' style={{ transform: `rotateX(${-rotateX}deg) rotateY(${rotateY}deg)` }}>
-                <div className='cardWrapper' ref={cardRef} style={{ transform: `scale(${scale})` }}>
-                    <div className="cardContainer">
+            <div className='rotationWrapper' style={{ transform: `rotateX(${-rotateX}deg) rotateY(${rotateY}deg)`, boxShadow: `${rotateY}px ${rotateX}px 80px 0px rgba(100, 100, 100, ${shadow})`}}>
+                <div className='cardWrapper' ref={cardRef} style={{ transform: `scale(${scale})`}}>
+                    <div className='sheen' style={{ backgroundImage:`linear-gradient(55deg, transparent, rgba(255 255 255 / ${sheenOpacity}) ${sheenPosition}%, transparent)`}}></div>
+                    <div className="cardContainer" >
                         <div className="imageContainer">
                             <img src={props.img} alt={props.name}/>
                         </div>
