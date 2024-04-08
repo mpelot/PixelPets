c78a63109a6656e81fa7d9c1797bf101298bdb70
Hannah <Hannah2520@gmail.com>
2024-04-07 22:41:24 -0400

    updated the home page

--- pixel-pets-app/src/app/home/page.js
+++ pixel-pets-app/src/app/home/page.js
@@ -15,11 +15,27 @@ export default function Home() {
   );
 
   return (
-    <div className='petContainer'>
-      <PetCard name={pet.name} img={pet.img} desc={pet.desc}></PetCard>
-      <PetCard name={pet.name} img={pet.img} desc={pet.desc}></PetCard>
-      <PetCard name={pet.name} img={pet.img} desc={pet.desc}></PetCard>
-      <PetCard name={pet.name} img={pet.img} desc={pet.desc}></PetCard>
+    <div className="petContainer">
+      <div className="titleContainer">
+        <div className="pixelPets">
+          <img src="logo.png" />
+          <h1 className="mainTitle">Pixel Pets</h1>
+        </div>
+
+        <button className="browse">Browse Pets</button>
+      </div>
+      <div className="subContainer">
+        <div className="subheader">
+          <h2 className="subtitle">My Pets</h2>
+        </div>
+
+        <div className="myCards">
+          <PetCard name={pet.name} img={pet.img} desc={pet.desc}></PetCard>
+          <PetCard name={pet.name} img={pet.img} desc={pet.desc}></PetCard>
+          <PetCard name={pet.name} img={pet.img} desc={pet.desc}></PetCard>
+          <PetCard name={pet.name} img={pet.img} desc={pet.desc}></PetCard>
+        </div>
+      </div>
     </div>
   );
 }
