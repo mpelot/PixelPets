98ac714e6b5c3bfba267470104e6536bc6691013
Hannah <Hannah2520@gmail.com>
2024-04-08 23:50:14 -0400

    changes to css

--- pixel-pets-app/src/app/addPets/page.js
+++ pixel-pets-app/src/app/addPets/page.js
@@ -28,7 +28,7 @@ export default function Home() {
   };
 
   return (
-    <div className='getPetsContainer'>
+    <div className="getPetsContainer">
       {/* <header>
         <h1>Pixel Pets</h1>
       </header> */}
@@ -37,20 +37,36 @@ export default function Home() {
         <form onSubmit={handleSubmit}>
           <div className="upload">
             <h3 className="newPetTitle">Create New Pet</h3>
-            <img src="https://static.thenounproject.com/png/187803-200.png" alt="Logo" />
+            <img
+              src="https://static.thenounproject.com/png/187803-200.png"
+              alt="Logo"
+            />
             <input type="file" />
           </div>
           <div>
             <label>Name:</label>
-            <input type="text" name="name" value={formData.name} onChange={handleInputChange}/>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
           </div>
           <div>
             <label>Description:</label>
-            <textarea name="description" value={formData.description} onChange={handleInputChange}/>
+            <textarea
+              name="description"
+              value={formData.description}
+              onChange={handleInputChange}
+            />
           </div>
           <div>
             <label>Rarity:</label>
-            <select name="rarity" value={formData.rarity} onChange={handleInputChange}>
+            <select
+              name="rarity"
+              value={formData.rarity}
+              onChange={handleInputChange}
+            >
               <option value="1">⭐️</option>
               <option value="2">⭐️⭐️</option>
               <option value="3">⭐️⭐️⭐️</option>
@@ -60,7 +76,11 @@ export default function Home() {
           </div>
           <div>
             <label>Personality Trait:</label>
-            <select name="personalityTrait" value={formData.personalityTrait} onChange={handleInputChange}>
+            <select
+              name="personalityTrait"
+              value={formData.personalityTrait}
+              onChange={handleInputChange}
+            >
               <option value="">Select One</option>
               <option value="friendly">Friendly</option>
               <option value="outgoing">Outgoing</option>
