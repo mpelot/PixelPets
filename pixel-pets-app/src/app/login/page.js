ad1a893d1fdcf3727346065ba0baa57ffdcf521c
mpelot <mwp81301@uga.edu>
2024-04-17 23:06:06 -0400

    added create account

--- pixel-pets-app/src/app/login/page.js
+++ pixel-pets-app/src/app/login/page.js
@@ -1,6 +1,7 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
+import { useState, useEffect } from 'react'
 import styles from "./login.css";
 import bg from '../../../public/LogoBG.png'
 
@@ -8,14 +9,37 @@ export default function Home() {
 
   const router = useRouter()
 
-  const handleSubmit = (e) => {
+  const [hasAccount, setHasAccount] = useState(true);
+
+  const handleLoginSubmit = (e) => {
     e.preventDefault();
-    localStorage.setItem('loggedIn', true);
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('loggedIn', true);
+    }
     router.push('/home')
   };
 
-  localStorage.setItem('loggedIn', false);
-  console.log(localStorage.getItem('loggedIn'));
+  const handleSignUpSubmit = (e) => {
+    e.preventDefault();
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('loggedIn', true);
+    }
+    router.push('/home')
+  };
+
+  const loginView = (e) => {
+    setHasAccount(true)
+  }
+
+  const signUpView = (e) => {
+    setHasAccount(false)
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('loggedIn', false);
+    }
+  }, [])
 
   return (
       <div className="content">
@@ -25,20 +49,43 @@ export default function Home() {
             <img src="logo.png"/>
             <h1>PixelPets</h1>
           </div>
+          {hasAccount ? 
           <div className="login-container">
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id="username" name="username" required/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" required/>
-                </div>
-                <button type="submit">Login</button>
-            </form>
-        </div>
+            <div className="login-BG">
+              <h2>Login</h2>
+              <form onSubmit={handleLoginSubmit}>
+                  <div className="form-group">
+                      <label htmlFor="username">Username</label>
+                      <input type="text" id="username" name="username" required/>
+                  </div>
+                  <div className="form-group">
+                      <label htmlFor="password">Password</label>
+                      <input type="password" id="password" name="password" required/>
+                  </div>
+                  <button type="submit">Login</button>
+              </form>
+            </div>
+            <button className="altOption" onClick={signUpView}>Create Account</button>
+          </div>
+          :
+          <div className="login-container">
+            <div className="login-BG">
+              <h2>Sign Up</h2>
+              <form onSubmit={handleSignUpSubmit}>
+                  <div className="form-group">
+                      <label htmlFor="username">Username</label>
+                      <input type="text" id="username" name="username" required/>
+                  </div>
+                  <div className="form-group">
+                      <label htmlFor="password">Password</label>
+                      <input type="password" id="password" name="password" required/>
+                  </div>
+                  <button type="submit">Create Account</button>
+              </form>
+            </div>
+            <button className="altOption" onClick={loginView}>Login</button>
+          </div>
+          }
       </div>
     </div>
   );
