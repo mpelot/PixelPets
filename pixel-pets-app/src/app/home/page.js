08b728ce022083ca5b51f454c30e35c43c9fbb76
Hannah <Hannah2520@gmail.com>
2024-04-24 23:03:26 -0400

    started admin page and fixed home page

--- pixel-pets-app/src/app/home/page.js
+++ pixel-pets-app/src/app/home/page.js
@@ -1,32 +1,29 @@
-'use client'
-
-import { useState } from 'react';
+"use client";
+import { useState } from "react";
 import styles from "./home.css";
 import PetCard from "../petCard/petCard";
 import Navbar from "../navbar/navbar";
 
-
 export default function Home() {
-
   const [pets, setPets] = useState([
     {
       id: "0",
-      name: 'Turtle',
+      name: "Turtle",
       img: "Turtle.png",
-      desc: "This is a turtle rahhhhhhh"
+      desc: "This is a turtle rahhhhhhh",
     },
     {
       id: "1",
-      name: 'Turtle',
+      name: "Turtle",
       img: "Turtle.png",
-      desc: "This is a turtle rahhhhhhh"
+      desc: "This is a turtle rahhhhhhh",
     },
     {
       id: "2",
-      name: 'Turtle',
+      name: "Turtle",
       img: "Turtle.png",
-      desc: "This is a turtle rahhhhhhh"
-    }
+      desc: "This is a turtle rahhhhhhh",
+    },
   ]);
 
   return (
@@ -37,17 +34,21 @@ export default function Home() {
           <img src="logo.png" />
           <h1 className="mainTitle">Pixel Pets</h1>
         </div>
-
         <button className="browse">Browse Pets</button>
       </div>
-      <div className="subContainer">
-        <div className="subheader">
-          <h2 className="subtitle">My Pets</h2>
-        </div>
 
+      <h2 className="subtitle">My Pets</h2>
+
+      <div className="subContainer">
         <div className="myCards">
-          {pets.map(pet => <PetCard key={pet.id} name={pet.name} img={pet.img} desc={pet.desc}></PetCard>
-        )}
+          {pets.map((pet) => (
+            <PetCard
+              key={pet.id}
+              name={pet.name}
+              img={pet.img}
+              desc={pet.desc}
+            ></PetCard>
+          ))}
         </div>
       </div>
     </div>
--- pixel-pets-app/src/app/myPets/page.js
+++ pixel-pets-app/src/app/myPets/page.js
@@ -1,9 +1,106 @@
+"use client";
 import styles from "./myPets.css";
+import { useState } from "react";
+import PetCard from "../petCard/petCard";
+import Navbar from "../navbar/navbar";
 
 export default function Home() {
+  const [pets, setPets] = useState([
+    {
+      id: "0",
+      name: "Turtle",
+      img: "Turtle.png",
+      desc: "This is a turtle rahhhhhhh",
+    },
+    {
+      id: "1",
+      name: "Turtle",
+      img: "Turtle.png",
+      desc: "This is a turtle rahhhhhhh",
+    },
+    {
+      id: "2",
+      name: "Turtle",
+      img: "Turtle.png",
+      desc: "This is a turtle rahhhhhhh",
+    },
+    {
+      id: "3",
+      name: "Turtle",
+      img: "Turtle.png",
+      desc: "This is a turtle rahhhhhhh",
+    },
+    {
+      id: "4",
+      name: "Turtle",
+      img: "Turtle.png",
+      desc: "This is a turtle rahhhhhhh",
+    },
+    {
+      id: "5",
+      name: "Turtle",
+      img: "Turtle.png",
+      desc: "This is a turtle rahhhhhhh",
+    },
+    {
+      id: "6",
+      name: "Turtle",
+      img: "Turtle.png",
+      desc: "This is a turtle rahhhhhhh",
+    },
+    {
+      id: "7",
+      name: "Turtle",
+      img: "Turtle.png",
+      desc: "This is a turtle rahhhhhhh",
+    },
+    {
+      id: "8",
+      name: "Turtle",
+      img: "Turtle.png",
+      desc: "This is a turtle rahhhhhhh",
+    },
+    {
+      id: "9",
+      name: "Turtle",
+      img: "Turtle.png",
+      desc: "This is a turtle rahhhhhhh",
+    },
+    {
+      id: "10",
+      name: "Turtle",
+      img: "Turtle.png",
+      desc: "This is a turtle rahhhhhhh",
+    },
+    {
+      id: "11",
+      name: "Turtle",
+      img: "Turtle.png",
+      desc: "This is a turtle rahhhhhhh",
+    },
+  ]);
+
+   const handleEdit = (id) => {
+       console.log(`Edit pet with ID: ${id}`);
+   };
   return (
-    <div>
-      <h1>MyPets</h1>
+    <div className="petContainer">
+      <Navbar />
+
+      <h2 className="subtitle">Admin Access: All Pets</h2>
+
+      <div className="subContainer">
+        <div className="myCards">
+          {pets.map((pet) => (
+            <PetCard
+              key={pet.id}
+              name={pet.name}
+              img={pet.img}
+              desc={pet.desc}
+            ></PetCard>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
